2edd8339ced17f86c09e454e2ec65d77ac2fdac8
Doty The Cat <dotywrt@gmail.com>
2025-07-28 21:06:42 +0800

    Delete luci-app-nft-ttl directory

--- luci-app-nft-ttl/files/usr/lib/lua/luci/controller/nftttl.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.nftttl", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/nftttl") then return end
-	entry({"admin", "modem", "nftttl"}, cbi("nftttl"), _("TTL Settings"), 60).acl_depends = { "luci-app-nft-ttl" }
-end
